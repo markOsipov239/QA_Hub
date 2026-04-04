968338485dfdaad0f6b6702eada1fee589014173
mosipov <m.osipov@next-on.pro>
2026-04-04 17:34:07 +0300

    text attachments

--- qa_hub_backend/src/main/kotlin/qa_hub/controller/testRuns/AttachmentsController.kt
+++ qa_hub_backend/src/main/kotlin/qa_hub/controller/testRuns/AttachmentsController.kt
@@ -12,13 +12,14 @@ import javax.imageio.ImageIO
 
 
 val attachmentsDir = System.getenv("ENV_ATTACHMENTS_DIR") ?: "${System.getProperty("user.home")}/QA_Hub"
-val imageDir ="${attachmentsDir}/Images"
+val imageDir = "${attachmentsDir}/Images"
+val textDir = "${attachmentsDir}/Text"
 
 @RestController
 @RequestMapping("/api/attachments")
 class AttachmentsController {
     @PostMapping("/images")
-    fun postAttachments(
+    fun postImageAttachments(
         @RequestParam project: String,
         @RequestParam testRunId: String,
         @RequestParam fullName: String,
@@ -46,9 +47,38 @@ class AttachmentsController {
         throw Exception("Failed to save an attachment")
     }
 
+    @PostMapping("/text")
+    fun postTextAttachments(
+        @RequestParam project: String,
+        @RequestParam testRunId: String,
+        @RequestParam fullName: String,
+        @RequestParam image: MultipartFile,
+        @RequestParam(required = false, defaultValue = "txt") extension: String
+    ): TestResultAttachment {
+        val path = "$textDir/testruns/$project/$testRunId/$fullName"
+        val directory = File(path)
+        if (!directory.exists()) {
+            directory.mkdirs()
+        }
+        val fileName = "${UUID.randomUUID()}.$extension"
+        val file = File("$path/$fileName")
+
+        val result = ImageIO.write(ImageIO.read(image.inputStream), extension, file)
+
+        if (result) {
+            return TestResultAttachment(
+                type = AttachmentTypes.text,
+                path = "/api/attachments/text/${project}/${testRunId}/${fullName}/${fileName}",
+                fileName = fileName
+            )
+        }
+
+        throw Exception("Failed to save an attachment")
+    }
+
 
     @GetMapping("/images/{project}/{testRunId}/{fullName}/{fileName}", produces = [MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE])
-    fun getAttachment(
+    fun getImageAttachment(
         @PathVariable project: String,
         @PathVariable testRunId: String,
         @PathVariable fullName: String,
@@ -60,6 +90,23 @@ class AttachmentsController {
         } else {
             MediaType.IMAGE_JPEG
         }
+
+        val filePath = "$imageDir/testruns/$project/$testRunId/$fullName/$fileName"
+        val file = File(filePath)
+
+        return ResponseEntity.ok()
+            .contentType(mediaType)
+            .body(file.readBytes())
+    }
+
+    @GetMapping("/text/{project}/{testRunId}/{fullName}/{fileName}", produces = [MediaType.TEXT_PLAIN_VALUE])
+    fun getTextAttachment(
+        @PathVariable project: String,
+        @PathVariable testRunId: String,
+        @PathVariable fullName: String,
+        @PathVariable fileName: String,
+    ): ResponseEntity<ByteArray> {
+        val mediaType =  MediaType.TEXT_PLAIN
 
         val filePath = "$imageDir/testruns/$project/$testRunId/$fullName/$fileName"
         val file = File(filePath)
