af51b58709129465c60821bee9541ec269f32d05
mosipov <m.osipov@next-on.pro>
2026-04-05 02:25:42 +0300

    text attachments

--- qa_hub_backend/src/main/kotlin/qa_hub/service/testResults/TestStepsService.kt
+++ qa_hub_backend/src/main/kotlin/qa_hub/service/testResults/TestStepsService.kt
@@ -51,6 +51,6 @@ class TestStepsService {
         var parentId: String?,
         var steps: List<TestStep> = listOf(),
         var result: String,
-        var duration: Double? = null
+        var duration: Int? = null
     )
 }
