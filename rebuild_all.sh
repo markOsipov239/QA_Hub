ea95c6d115559a72169fdfe16c3bf834d4dbd149
mosipov <m.osipov@next-on.pro>
2026-04-26 19:42:06 +0300

    Merge docker compose files

--- rebuild_all.sh
+++ rebuild_all.sh
@@ -2,12 +2,11 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $SCRIPT_DIR/qa_hub_backend
+cd $SCRIPT_DIR
 docker stop qa_hub_backend
 docker rmi qa_hub_backend --force
-docker compose up -d
 
-cd $SCRIPT_DIR/qa_hub_frontend
 docker stop qa_hub_frontend
 docker rmi qa_hub_frontend --force
+
 docker compose up -d
